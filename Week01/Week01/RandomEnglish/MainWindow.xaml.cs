aa9e5bffd4f4775db30f87560c90d30f3d9f2f43
thole20042001 <nhattho03@gmail.com>
2021-09-16 20:10:28 +0700

    Add doc folder & week01

---
+++ Week01/Week01/RandomEnglish/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomEnglish
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeImage_Click(object sender, RoutedEventArgs e)
+        {
+            string[] names = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
+            string[] descs = { "Rabbit",
+                               "Cat",
+                               "Bird",
+                               "Frog",
+                               "Deer",
+                               "Owl",
+                               "Fish",
+                               "Bat",
+                               "Swan",
+                               "Penguin",
+                               "Butterfly",
+                               "Wolf"};
+
+            Random rng = new Random();
+            int i = rng.Next() % names.Length;
+            string name = names[i];
+
+            var bitmap = new BitmapImage(
+                new Uri($"/img/english{name}.jpg", UriKind.Relative));
+            imgQuote.Source = bitmap;
+            quoteDesc.Text = descs[i];
+        }
+    }
+}
---
+++ Week01/demo/HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeImage_Click(object sender, RoutedEventArgs e)
+        {
+            string[] names = { "01", "02", "03", "04", "05", "06" };
+            Random rng = new Random();
+            int i = rng.Next() % names.Length;
+            string name = names[i];
+
+            var bitmap = new BitmapImage(
+                new Uri($"/Images/avatar{name}.jpg", UriKind.Relative));
+            imgAvatar.Source = bitmap;
+
+            
+        }
+    }
+}
